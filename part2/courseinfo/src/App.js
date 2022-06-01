57ad67d2d3390198cc50c1a158b4a898d994a871
Ryan Murray <105310431+murray-dev@users.noreply.github.com>
2022-06-01 18:05:30 +0200

    part2/courseinfo exercise 2.5

--- part2/courseinfo/src/App.js
+++ part2/courseinfo/src/App.js
@@ -1,31 +1,4 @@
-const Header = ({ course }) => <h2>{course}</h2>;
-
-const Content = ({ parts }) => (
-  <>
-    {parts.map((part) => <Part part={part} key={part.id} />)}
-  </>
-);
-
-const Part = ({ part }) => <p>{part.name} {part.exercises}</p>;
-
-const Total = ({ parts }) => {
-
-  const sum = parts.reduce((sum, part) => sum + part.exercises, 0);
-
-  return (
-    <>
-      <p><strong>total of {sum} exercises</strong></p>
-    </>
-  )
-};
-
-const Course = ({ course }) => (
-  <div>
-    <Header course={course.name} />
-    <Content parts={course.parts} />
-    <Total parts={course.parts} />
-  </div>
-);
+import Course from './components/Course';
 
 const App = () => {
   const courses = [
