855b28c6522ca3596b1c32a97b3f550ffedeed19
Ryan Murray <105310431+murray-dev@users.noreply.github.com>
2022-06-02 19:22:17 +0200

    part2/phonebook exercise 2.6

---
+++ part2/phonebook/src/App.js
@@ -0,0 +1,37 @@
+import { useState } from 'react'
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas' }
+  ])
+  const [newName, setNewName] = useState('')
+
+  const handleNameChange = (event) => setNewName(event.target.value)
+
+  const addContact = (event) => {
+    event.preventDefault()
+    const newContact = {
+      name: newName
+    }
+    setPersons(persons.concat(newContact))
+    setNewName('')
+  }
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addContact}>
+        <div>
+          name: <input value={newName} onChange={handleNameChange} />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      {persons.map((x) => <p key={x.name}>{x.name}</p>)}
+    </div>
+  )
+}
+
+export default App
