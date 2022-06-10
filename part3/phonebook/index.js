938447395bfc571643d70e50e420bf9d4b6874f0
Ryan Murray <105310431+murray-dev@users.noreply.github.com>
2022-06-10 18:42:28 +0200

    part3/phonebook exercise 3.5

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
 
+app.use(express.json());
+
 let persons = [
   {
     "id": 1,
@@ -51,6 +53,15 @@ app.delete('/api/persons/:id', (request, response) => {
   persons = persons.filter(person => person.id !== id);
 
   response.status(204).end();
+});
+
+app.post('/api/persons', (request, response) => {
+  const person = request.body;
+  person.id = Math.floor(Math.random() * 10 ** 12);
+
+  persons = persons.concat(person);
+
+  response.json(person);
 });
 
 const PORT = 3001;
