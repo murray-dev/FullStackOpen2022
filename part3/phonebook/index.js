d09d93e8830a0a21671a5467a4ddc3a9fc609006
Ryan Murray <105310431+murray-dev@users.noreply.github.com>
2022-06-10 17:15:29 +0200

    part3/phonebook exercise 3.2

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -24,6 +24,13 @@ let persons = [
     }
 ];
 
+app.get('/info', (request, response) => {
+  response.send(
+    `<p>Phonebook has info for ${persons.length} people</p>
+    <p>${Date()}</p>`
+  );
+});
+
 app.get('/api/persons', (request, response) => {
     response.json(persons);
 });
