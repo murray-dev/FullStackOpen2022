f296f03955bfc55477e3936c386d5779d0844019
Ryan Murray <105310431+murray-dev@users.noreply.github.com>
2022-06-22 23:23:30 +0200

    part3/phonebook exercise 3.15

--- part3/phonebook/backend/index.js
+++ part3/phonebook/backend/index.js
@@ -23,11 +23,10 @@ app.get('/api/persons/:id', (request, response) =>
     .catch(e => response.status(404).end())
 );
 
-app.delete('/api/persons/:id', (request, response) => {
-  const id = Number(request.params.id);
-  persons = persons.filter(person => person.id !== id);
-
-  response.status(204).end();
+app.delete('/api/persons/:id', (request, response, next) => {
+  Person.findByIdAndDelete(request.params.id)
+    .then(() => response.status(204).end())
+    .catch(error => next(error));
 });
 
 app.post('/api/persons', (request, response) => {
