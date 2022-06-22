e47ef2e695faf60337c1b2750aa52ca709475f7a
Ryan Murray <105310431+murray-dev@users.noreply.github.com>
2022-06-23 00:47:14 +0200

    part3/phonebook exercise 3.16

--- part3/phonebook/backend/index.js
+++ part3/phonebook/backend/index.js
@@ -1,4 +1,5 @@
 require("dotenv").config();
+const { response } = require("express");
 const express = require("express");
 const { default: mongoose } = require("mongoose");
 const morgan = require("morgan");
@@ -13,14 +14,22 @@ app.use(
   morgan(':method :url :status :res[content-length] - :response-time ms :body')
 );
 
-app.get('/api/persons', (request, response) =>
-  Person.find({}).then(persons => response.json(persons))
+app.get('/api/persons', (request, response, next) =>
+  Person.find({})
+    .then(persons => response.json(persons))
+    .catch(error => next(error))
 );
 
-app.get('/api/persons/:id', (request, response) =>
+app.get('/api/persons/:id', (request, response, next) =>
   Person.findById(request.params.id)
-    .then(person => response.json(person))
-    .catch(e => response.status(404).end())
+    .then((person) => {
+      if (person) {
+        response.json(person);
+      } else {
+        response.status(404).end();
+      }
+    })
+    .catch(error => next(error))
 );
 
 app.delete('/api/persons/:id', (request, response, next) => {
@@ -29,7 +38,7 @@ app.delete('/api/persons/:id', (request, response, next) => {
     .catch(error => next(error));
 });
 
-app.post('/api/persons', (request, response) => {
+app.post('/api/persons', (request, response, next) => {
   const body = request.body
 
   if (!body.name || !body.number) {
@@ -43,8 +52,21 @@ app.post('/api/persons', (request, response) => {
     number: body.number,
   });
 
-  person.save().then(savedPerson => response.json(savedPerson));
+  person.save()
+    .then(savedPerson => response.json(savedPerson))
+    .catch(error => next(error));
 });
+
+const errorHandler = (error, request, response, next) => {
+  console.log(error.message);
+
+  if (error.name === "CastError") {
+    return response.status(400).send({ error: "Invalid id" });
+  }
+
+  next(error);
+}
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
